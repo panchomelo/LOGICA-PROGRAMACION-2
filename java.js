7dc5585f1eb53b966400f2b717d56aa2571e4ea7
franciscovega <fran.vgasalv@gmail.com>
2024-02-05 02:32:29 -0300

    commit final

--- java.js
+++ java.js
@@ -1,1 +1,13 @@
-let temperatura = parseFloat(prompt("ingrese temperatura en grados Celsius"))
+function obtenerCelsius() {
+  let valorCelsius = document.getElementById("Celsius").value;
+  let CelsiusFloat = parseFloat(valorCelsius);
+
+  if (!isNaN(CelsiusFloat)) {
+    let gradosKelvin = CelsiusFloat + 273.15;
+    let gradosFarenheit = CelsiusFloat * 1.8 + 32;
+    alert(`la temperatura en gradius Kelvin es ${gradosKelvin}`);
+    alert(`la temperatura en grados Farenheit es ${gradosFarenheit}`);
+  } else {
+    alert("dato incorrecto, por favor ingrese un número");
+  }
+}
